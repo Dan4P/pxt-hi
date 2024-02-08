5670f7a673bb72c7d6859332c263fea5c9f56757
Dan4P <157136699+Dan4P@users.noreply.github.com>
2024-02-08 20:13:44 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
-basic.forever(function () {
-	
-})
+namespace hello {
+    //%block
+    export function showHi() {
+        led.toggle(0, 0)
+        led.toggle(0, 1)
+        led.toggle(0, 2)
+        led.toggle(0, 3)
+        led.toggle(0, 4)
+        led.toggle(1, 2)
+        led.toggle(2, 4)
+        led.toggle(2, 3)
+        led.toggle(2, 2)
+        led.toggle(4, 0)
+        led.toggle(4, 2)
+        led.toggle(4, 3)
+        led.toggle(4, 4)
+
+
+
+    }
+}
